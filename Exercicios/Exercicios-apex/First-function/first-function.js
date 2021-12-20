5e4ec29128c9a9c8d352052cd163aa8f45d54ca5
KevynDalsochio <95386292+KevynDalsochio@users.noreply.github.com>
2021-12-19 21:03:09 -0300

    Add files via upload

---
+++ Exercicios/Exercicios-apex/First-function/first-function.js
@@ -0,0 +1,12 @@
+
+function sum() {
+  let number1 = parseFloat(document.getElementById("userNum1").value)
+  let number2 = parseFloat(document.getElementById("userNum2").value)
+  let number3 = parseFloat(document.getElementById("userNum3").value)
+  let number4= parseFloat(document.getElementById("userNum4").value)
+  let number5 = parseFloat(document.getElementById("userNum5").value)
+  document.write((number1 + number2+ number3+ number4+ number5));
+}
+
+
+
